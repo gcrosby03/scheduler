b1470bc36ae7eb4702c34b730e685d7d665e943b
gcrosby03 <garrettcrosby.aas@gmail.com>
2022-09-19 20:40:15 -0500

    Finished project

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,8 +1,7 @@
-var data = []
+var data = JSON.parse(localStorage.getItem('dataStuff')) || [];
 
 var today = moment();
 $('#currentDay').text(today.format("dddd, MMM Do, YYYY"));
-console.log(today);
 
 var timelineLabels = (desiredStartTime, interval, period) => {
     const periodsInADay = moment.duration(8, 'hours').as(period);
@@ -17,20 +16,46 @@ var timelineLabels = (desiredStartTime, interval, period) => {
     return timeLabels;
 };
 
-
 function buildTimeTable() {
     var timeLabels = timelineLabels(9, 1, 'hours');
-    for (let i = 0; i < timeLabels.length; i++) {
-        data.push({ time: timeLabels[i], textfield: '' });
+    
+    if (data.length === 0) {
+
+        for (let i = 0; i < timeLabels.length; i++) {
+            var cssClass = "";
+
+            if (parseInt(moment(timeLabels[i], "h A").format("H")) === parseInt(today.format("H"))) {
+                cssClass = "present";
+            }
+            else if (parseInt(moment(timeLabels[i], "h A").format("H")) > parseInt(today.format("H"))) {
+                cssClass = "future";
+            }
+            else {
+                cssClass = "past";
+            }
+            data.push({ time: timeLabels[i], textfield: '', class: cssClass })
+
+        }
     }
     for (let i = 0; i < data.length; i++) {
         $('#timeField').append(`<div class="row">
-        <div class="col-1 hour d-flex align-items-center justify-content-end" id="${data[i].time}">${data[i].time}</div>
-        <div class="col-10" id="nineLine">${data[i].textfield}</div>
-        <div class="col-1 saveBtn d-flex align-items-center justify-content-center"><i class="far fa-save fa-3x"></i></div>
-      </div>`);
+        <div class="col-sm-1 hour d-flex align-items-center justify-content-end" id="${data[i].time}">${data[i].time}</div>
+        <div class="col-sm-10" id="nineLine"><input value = "${data[i].textfield}" type = "text" class = "${data[i].class} form-control form-control-sm h-100" id = "inputField" data-number=${i} /></div>
+        <div class="col-sm-1 saveBtn d-flex align-items-center justify-content-center" data-number=${i}><i class="far fa-save fa-3x"></i></div>
+        </div>`);
     }
 
 }
-buildTimeTable();
 
+buildTimeTable();
+var saveBtns = Array.from(document.querySelectorAll('.saveBtn'));
+
+
+saveBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        var inputFields = Array.from(document.querySelectorAll('#inputField'));
+        data[e.currentTarget.dataset['number']].textfield = inputFields[e.currentTarget.dataset['number']].value;
+        localStorage.setItem('dataStuff', JSON.stringify(data))
+        $('.saveConfirmation').removeClass('d-none');
+    })
+});
